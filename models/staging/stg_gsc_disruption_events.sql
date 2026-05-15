b2006cde009c0a47e779e6c624d48c5bb8770ad8
Matthew Scott <matthewscott364@gmail.com>
2026-05-15 13:29:44 -0400

    staging models progress

---
+++ models/staging/stg_gsc_disruption_events.sql
@@ -0,0 +1,13 @@
+SELECT 
+    CAST(DATE AS DATE) AS disruption_date,
+    'year',
+    LOWER(disruption_type) AS disruption_type,
+    LOWER(region_affected) AS region_affected,
+    duration_days,
+    severity,
+    CAST(is_pandemic AS BOOLEAN) AS is_pandemic,
+    CAST(is_geopolitical AS BOOLEAN) as is_geopolitical,
+    CAST(port_closure AS BOOLEAN) as port_closure,
+    freight_cost_impact_pct,
+    trade_volume_impact_pct
+FROM {{ source('cleaned_data', 'gsc_disruption_events_clean') }}
---
+++ models/staging/stg_gsc_shipping_rates.sql
@@ -0,0 +1,21 @@
+SELECT 
+    CAST(DATE AS DATE) as shipping_date,
+    'year',
+    'month',
+    'route',
+    shipping_mode,
+    CAST(container_rate_usd AS NUMERIC) as container_rate_usd,
+    CAST(baltic_dry_index AS NUMERIC) as baltic_dry_index,
+    CAST(air_freight_index AS NUMERIC) as air_freight_index,
+    CAST(supply_chain_pressure_index AS NUMERIC) as supply_chain_pressure_index,
+    CASE
+        WHEN supply_chain_pressure_index >= 2 THEN 'severe'
+        WHEN supply_chain_pressure_index >= 1 THEN 'elevated'
+        ELSE 'normal'
+    END AS pressure_level,
+    CASE
+        WHEN container_rate_usd > 10000 THEN 'extreme'
+        WHEN container_rate_usd > 5000 THEN 'high'
+        ELSE 'stable'
+    END AS freight_market_condition
+FROM {{ source('cleaned_data', 'gsc_shipping_rates_clean') }}
